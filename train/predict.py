e6bfd6604a63183c6faeb59bb44ab8328dbc9517
James W. McClain <jmcclain@daystrom-data-concepts.com>
2021-08-24 22:14:45 -0400

    Prediction

---
+++ train/predict.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import tqdm
+import numpy as np
+import torch
+import torch.hub
+import warnings
+import copy
+import rasterio as rio
+from rasterio.windows import Window
+import sys
+
+import torch
+import torch.hub
+
+
+def cli_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--backbone', required=True, type=str)
+    parser.add_argument('--chunksize', required=False, type=int, default=256)
+    parser.add_argument('--imagery', required=True, choices=['aviris', 'sentinel2'])
+    parser.add_argument('--infile', required=True, type=str)
+    parser.add_argument('--outfile', required=True, type=str)
+    parser.add_argument('--pth-load', required=True, type=str)
+    parser.add_argument('--stride', required=False, type=int, default=13)
+    parser.add_argument('--window-size', required=False, type=int, default=32)
+
+    parser.add_argument('--cheaplab', dest='cheaplab', action='store_true')
+    parser.add_argument('--no-cheaplab', dest='cheaplab', action='store_false')
+    parser.set_defaults(cheaplab=True)
+
+    return parser
+
+
+if __name__ == '__main__':
+
+    warnings.filterwarnings('ignore')
+
+    args = cli_parser().parse_args()
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+    log = logging.getLogger()
+
+    device = torch.device('cuda')
+    model = torch.hub.load('jamesmcclain/algae-classifier:master',
+                           'make_algae_model',
+                           imagery=args.imagery,
+                           use_cheaplab=args.cheaplab,
+                           backbone_str=args.backbone)
+    model.load_state_dict(torch.load(args.pth_load))
+    model.to(device)
+    model.eval()
+
+    with rio.open(args.infile, 'r') as infile_ds, torch.no_grad():
+        out_raw_profile = copy.deepcopy(infile_ds.profile)
+        out_raw_profile.update({
+            'compress': 'lzw',
+            'dtype': np.float32,
+            'count': 1,
+            'bigtiff': True,
+        })
+        width = infile_ds.width
+        height = infile_ds.height
+        ar_out = torch.zeros((1, height, width), dtype=torch.float32).to(device)
+        pixel_hits = torch.zeros((1, height, width), dtype=torch.uint8).to(device)
+
+        if args.imagery == 'aviris':
+            indexes = list(range(1,224+1))
+        elif args.imagery == 'sentinel2':
+            indexes = list(range(1,12+1))
+        n = args.window_size
+
+        # gather up batches
+        batches = []
+        for i in range(0, width-n, args.stride):
+            for j in range(0, height-n, args.stride):
+                batches.append((i, j))
+        batches = [batches[i:i + args.chunksize] for i in range(0, len(batches), args.chunksize)]
+
+        for batch in tqdm.tqdm(batches):
+            windows = [infile_ds.read(indexes, window=Window(i, j, n, n)) for (i, j) in batch]
+            windows = [w.astype(np.float32) for w in windows]
+            try:
+                windows = np.stack(windows, axis=0)
+            except:
+                continue
+            windows = torch.from_numpy(windows).to(dtype=torch.float32, device=device)
+            prob = torch.sigmoid(model(windows))
+
+            for k, (i, j) in enumerate(batch):
+                ar_out[0, j:(j+n), i:(i+n)] += prob[k]
+                pixel_hits[0, j:(j+n), i:(i+n)] += 1
+
+    # Bring results back to CPU
+    ar_out /= pixel_hits
+    ar_out = ar_out.cpu().numpy()
+
+    # Write results to file
+    with rio.open(args.outfile, 'w', **out_raw_profile) as outfile_raw_ds:
+        outfile_raw_ds.write(ar_out[0], indexes=1, window=Window(0, 0, width, height))
--- train/train.py
+++ train/train.py
@@ -75,126 +75,6 @@ def entropy_function(x, w=1e-1):
                 (pno_narrow * torch.log(pno_wide)))
 
 
-class AlgaeClassifier(torch.nn.Module):
-    def __init__(self,
-                 imagery: str = 'aviris',
-                 use_cheaplab: bool = True,
-                 backbone_str: str = None):
-        super().__init__()
-
-        self.imagery = imagery
-        self.use_cheaplab = use_cheaplab
-        self.backbone_str = backbone_str
-
-        # Number of input bands
-        if self.imagery == 'aviris':
-            n = 224
-        elif self.imagery == 'sentinel2':
-            n = 12
-        else:
-            raise Exception(f'unknown imagery type {self.imagery}')
-
-        # Number of bands going into the backbone
-        if self.use_cheaplab:
-            m = 2
-            self.cheaplab = torch.hub.load('jamesmcclain/CheapLab:08d260b',
-                                           'make_cheaplab_model',
-                                           num_channels=n)
-        else:
-            m = n
-
-        backbone = getattr(tv.models, self.backbone_str)
-        self.backbone = backbone(pretrained=True)
-
-        # Input and output
-        # if self.backbone_str == 'alexnet':
-        #     self.first = self.backbone.features[0] = torch.nn.Conv2d(
-        #         m, 64, kernel_size=(11, 11), stride=(4, 4), padding=(2, 2))
-        #     self.last = self.backbone.classifier[6] = torch.nn.Linear(
-        #         in_features=4096, out_features=1, bias=True)
-        if self.backbone_str == 'vgg16':
-            self.first = self.backbone.features[0] = torch.nn.Conv2d(
-                m, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-            self.last = self.backbone.classifier[6] = torch.nn.Linear(
-                in_features=4096, out_features=1, bias=True)
-        elif self.backbone_str == 'squeezenet1_0':
-            self.first = self.backbone.features[0] = torch.nn.Conv2d(
-                m, 96, kernel_size=(7, 7), stride=(2, 2))
-            self.last = self.backbone.classifier[1] = torch.nn.Conv2d(
-                512, 1, kernel_size=(1, 1), stride=(1, 1))
-        elif self.backbone_str == 'densenet161':
-            self.first = self.backbone.features.conv0 = \
-                torch.nn.Conv2d(
-                    m,
-                    96,
-                    kernel_size=(7, 7),
-                    stride=(2, 2),
-                    padding=(3, 3),
-                    bias=False)
-            self.last = self.backbone.classifier = torch.nn.Linear(
-                in_features=2208, out_features=1, bias=True)
-        elif self.backbone_str == 'shufflenet_v2_x1_0':
-            self.first = self.backbone.conv1[0] = torch.nn.Conv2d(
-                m,
-                24,
-                kernel_size=(3, 3),
-                stride=(2, 2),
-                padding=(1, 1),
-                bias=False)
-            self.last = self.backbone.fc = torch.nn.Linear(in_features=1024,
-                                                           out_features=1,
-                                                           bias=True)
-        elif self.backbone_str == 'mobilenet_v2':
-            self.first = self.backbone.features[0][0] = torch.nn.Conv2d(
-                m,
-                32,
-                kernel_size=(3, 3),
-                stride=(2, 2),
-                padding=(1, 1),
-                bias=False)
-            self.last = self.backbone.classifier[1] = torch.nn.Linear(
-                in_features=1280, out_features=1, bias=True)
-        elif self.backbone_str in ['mobilenet_v3_large', 'mobilenet_v3_small']:
-            self.first = self.backbone.features[0][0] = torch.nn.Conv2d(
-                m,
-                16,
-                kernel_size=(3, 3),
-                stride=(2, 2),
-                padding=(1, 1),
-                bias=False)
-            in_features = self.backbone.classifier[0].out_features
-            self.last = self.backbone.classifier[3] = torch.nn.Linear(
-                in_features=in_features, out_features=1, bias=True)
-        elif self.backbone_str == 'mnasnet1_0':
-            self.first = self.backbone.layers[0] = torch.nn.Conv2d(
-                m,
-                32,
-                kernel_size=(3, 3),
-                stride=(2, 2),
-                padding=(1, 1),
-                bias=False)
-            self.last = self.backbone.classifier[1] = torch.nn.Linear(
-                in_features=1280, out_features=1, bias=True)
-        elif self.backbone_str in ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152']:
-            self.first = self.backbone.conv1 = torch.nn.Conv2d(m,
-                                                               64,
-                                                               kernel_size=(7, 7),
-                                                               stride=(2, 2),
-                                                               padding=(3, 3),
-                                                               bias=False)
-            in_features = self.backbone.fc.in_features
-            self.last = self.backbone.fc = torch.nn.Linear(in_features, 1)
-        else:
-            raise Exception(f'Unknown backbone {self.backbone_str}')
-
-    def forward(self, x):
-        out = x
-        if self.use_cheaplab:
-            out = self.cheaplab(out)
-        out = self.backbone(out)
-        return out
-
-
 class AlgaeDataset(torch.utils.data.Dataset):
     def __init__(self, savez):
         npz = np.load(savez)
@@ -222,9 +102,11 @@ if __name__ == '__main__':
     log = logging.getLogger()
 
     device = torch.device('cuda')
-    model = AlgaeClassifier(imagery=args.imagery,
-                            use_cheaplab=args.cheaplab,
-                            backbone_str=args.backbone)
+    model = torch.hub.load('jamesmcclain/algae-classifier:master',
+                           'make_algae_model',
+                           imagery=args.imagery,
+                           use_cheaplab=args.cheaplab,
+                           backbone_str=args.backbone)
     model.to(device)
 
     opt = torch.optim.AdamW(model.parameters(), lr=args.lr)
@@ -295,6 +177,7 @@ if __name__ == '__main__':
     else:
         log.info(f'Loading model from {args.pth_load}')
         model.load_state_dict(torch.load(args.pth_load))
+        model.to(device)
 
     log.info('Training everything')
     unfreeze(model.backbone)
