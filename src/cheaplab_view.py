5e9baddd16e8bbe1a6771a2f374b838772d50fd4
James McClain <jmcclain@azavea.com>
2021-08-27 15:21:40 -0400

    CheapLab Viewer (#9)

--- src/cheaplab_view.py
+++ src/cheaplab_view.py
@@ -40,24 +40,7 @@ def cli_parser():
     parser.add_argument('--infile', required=True, type=str)
     parser.add_argument('--outfile', required=True, type=str)
     parser.add_argument('--pth-load', required=True, type=str)
-    parser.add_argument('--stride', required=False, type=int, default=13)
     parser.add_argument('--window-size', required=False, type=int, default=32)
-
-    parser.add_argument('--ndwi-mask',
-                        required=False,
-                        dest='ndwi_mask',
-                        action='store_true')
-    parser.set_defaults(ndwi_mask=False)
-
-    parser.add_argument('--cloud-hack',
-                        required=False,
-                        dest='cloud_hack',
-                        action='store_true')
-    parser.set_defaults(cloud_hack=False)
-
-    parser.add_argument('--no-cheaplab', dest='cheaplab', action='store_false')
-    parser.set_defaults(cheaplab=True)
-
     return parser
 
 
@@ -69,62 +52,56 @@ if __name__ == '__main__':
     logging.basicConfig(stream=sys.stderr, level=logging.INFO)
     log = logging.getLogger()
 
+    n = args.window_size
+
     device = torch.device(args.device)
     model = torch.hub.load('jamesmcclain/algae-classifier:master',
                            'make_algae_model',
                            imagery=args.imagery,
-                           use_cheaplab=args.cheaplab,
+                           use_cheaplab=True,
                            backbone_str=args.backbone,
                            pretrained=False)
     model.load_state_dict(torch.load(args.pth_load))
+    model = model.cheaplab
     model.to(device)
     model.eval()
 
+    # Read data
     with rio.open(args.infile, 'r') as infile_ds, torch.no_grad():
         out_raw_profile = copy.deepcopy(infile_ds.profile)
         out_raw_profile.update({
             'compress': 'lzw',
             'dtype': np.float32,
-            'count': 1,
+            'count': 3,
             'bigtiff': True,
         })
         width = infile_ds.width
         height = infile_ds.height
-        ar_out = torch.zeros((1, height, width),
-                             dtype=torch.float32).to(device)
-        pixel_hits = torch.zeros((1, height, width),
-                                 dtype=torch.uint8).to(device)
+        data_out = torch.zeros((3, height, width),
+                               dtype=torch.float32).to(device)
 
         if args.imagery == 'aviris':
             indexes = list(range(1, 224 + 1))
         elif args.imagery == 'sentinel2':
             indexes = list(range(1, 12 + 1))
-        n = args.window_size
 
-        # gather up batches
+        # Gather up batches
         batches = []
-        for i in range(0, width - n, args.stride):
-            for j in range(0, height - n, args.stride):
+        for i in range(0, width - n, n):
+            for j in range(0, height - n, n):
                 batches.append((i, j))
         batches = [
             batches[i:i + args.chunksize]
             for i in range(0, len(batches), args.chunksize)
         ]
 
+        # Evaluate batches
         for batch in tqdm.tqdm(batches):
             windows = [
                 infile_ds.read(indexes, window=Window(i, j, n, n))
                 for (i, j) in batch
             ]
             windows = [w.astype(np.float32) for w in windows]
-            if args.ndwi_mask:
-                windows = [
-                    w * (((w[2] - w[7]) / (w[2] + w[7])) > 0.0)
-                    for w in windows
-                ]
-            if args.cloud_hack:
-                windows = [(w * (w[3] > 100) * (w[3] < 1000)) for w in windows]
-
             try:
                 windows = np.stack(windows, axis=0)
             except:
@@ -134,15 +111,11 @@ if __name__ == '__main__':
             prob = torch.sigmoid(model(windows))
 
             for k, (i, j) in enumerate(batch):
-                ar_out[0, j:(j + n), i:(i + n)] += prob[k]
-                pixel_hits[0, j:(j + n), i:(i + n)] += 1
+                data_out[:, j:(j + n), i:(i + n)] = prob[k]
 
-    # Bring results back to CPU
-    ar_out /= pixel_hits
-    ar_out = ar_out.cpu().numpy()
+        # Bring results back to CPU
+        data_out = data_out.cpu().numpy()
 
     # Write results to file
     with rio.open(args.outfile, 'w', **out_raw_profile) as outfile_raw_ds:
-        outfile_raw_ds.write(ar_out[0],
-                             indexes=1,
-                             window=Window(0, 0, width, height))
+        outfile_raw_ds.write(data_out)
