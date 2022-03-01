e6b3eaf8bd5b4dc01ca3a6346205381d86000448
James W. McClain <jmcclain@daystrom-data-concepts.com>
2022-03-01 12:57:35 -0500

    Update Viewer

--- src/train_seg.py
+++ src/train_seg.py
@@ -7,6 +7,7 @@ import sys
 
 import numpy as np
 import torch
+from torch import nn
 import torchvision as tv
 import tqdm
 from torch.optim.lr_scheduler import OneCycleLR
@@ -21,11 +22,15 @@ def cli_parser():
     parser.add_argument('--batch-size', required=False, type=int, default=8)
     parser.add_argument('--epochs', required=False, type=int, default=17)
     parser.add_argument('--lr', required=False, type=float, default=1e-3)
-    parser.add_argument('--num-workers', required=False, type=int, default=2)
+    parser.add_argument('--num-workers', required=False, type=int, default=1)
     parser.add_argument('--pth-load', required=False, type=str)
     parser.add_argument('--pth-save', required=False, type=str, default='model.pth')
     parser.add_argument('--sentinel-l1c-path', required=False, type=str, default=None)
     parser.add_argument('--sentinel-l2a-path', required=False, type=str, default=None)
+
+    parser.add_argument('--freeze-bn', required=False, dest='freeze_bn', action='store_true')
+    parser.set_defaults(freeze_bn=False)
+
     return parser
 
 
@@ -37,6 +42,36 @@ def worker_init_fn(x):
 def collate_fn(batch):
     batch = list(filter(lambda x: x is not None, batch))
     return torch.utils.data.dataloader.default_collate(batch)
+
+
+def freeze(m: nn.Module) -> nn.Module:
+    for p in m.parameters():
+        p.requires_grad = False
+
+
+def unfreeze(m: nn.Module) -> nn.Module:
+    for p in m.parameters():
+        p.requires_grad = True
+
+
+def freeze_bn(m):
+    for (name, child) in m.named_children():
+        if isinstance(child, torch.nn.BatchNorm2d):
+            for param in child.parameters():
+                param.requires_grad = False
+            child.eval()
+        else:
+            freeze_bn(child)
+
+
+def unfreeze_bn(m):
+    for (name, child) in m.named_children():
+        if isinstance(child, torch.nn.BatchNorm2d):
+            for param in child.parameters():
+                param.requires_grad = True
+            child.train()
+        else:
+            unfreeze_bn(child)
 
 
 dataloader_cfg = {
@@ -100,6 +135,8 @@ if __name__ == '__main__':
     if args.pth_load is not None:
         model.load_state_dict(torch.load(args.pth_load), strict=True)
     device = torch.device('cuda')
+    log.info(f'freeze_bn={args.freeze_bn}')
+
     model.to(device)
 
     opt = torch.optim.AdamW(model.parameters(), lr=args.lr)
@@ -114,21 +151,24 @@ if __name__ == '__main__':
         dl = choice.get('dl')
         shadows = choice.get('shadows')
         model.train()
+        if args.freeze_bn and i > 0:
+            freeze_bn(model)
         for (j, batch) in tqdm.tqdm(enumerate(dl), total=len(dl), desc='Training'):
             out = model(batch[0].to(device))
             if shadows:
                 cloud_gt = (batch[1] == 2).type(out.type())
                 cloud_shadow_gt = (batch[1] == 3).type(out.type())
-                cloud_pred = out[:, 1, :, :] - 0.5*(out[:, 0, :, :] + out[:, 2, :, :])
-                cloud_shadow_pred = out[:, 2, :, :] - 0.5*(out[:, 0, :, :] + out[:, 1, :, :])
-                loss = obj_bce(cloud_pred, cloud_gt.to(device))
-                loss += obj_bce(cloud_shadow_pred, cloud_shadow_gt.to(device))
+                cloud_pred = out[:, 1, :, :] - out[:, 0, :, :]
+                cloud_shadow_pred = out[:, 2, :, :] - out[:, 0, :, :]
+                loss1 = obj_bce(cloud_pred, cloud_gt.to(device))
+                loss2 = obj_bce(cloud_shadow_pred, cloud_shadow_gt.to(device))
+                loss = loss1 + loss2
             else:
                 cloud_gt = (batch[1] == 1).type(out.type())
-                cloud_pred = out[:, 1, :, :] - out[:, 0:, :, :]
+                cloud_pred = out[:, 1, :, :] - out[:, 0, :, :]
                 loss = obj_bce(cloud_pred, cloud_gt.to(device))
-            train_losses.append(loss.item())
             loss.backward()
+            train_losses.append(loss.item())
             opt.step()
             opt.zero_grad()
         avg_train_loss = np.mean(train_losses)
@@ -145,13 +185,14 @@ if __name__ == '__main__':
                 if shadows:
                     cloud_gt = (batch[1] == 2).type(out.type())
                     cloud_shadow_gt = (batch[1] == 3).type(out.type())
-                    cloud_pred = out[:, 1, :, :] - 0.5*(out[:, 0, :, :] + out[:, 2, :, :])
-                    cloud_shadow_pred = out[:, 2, :, :] - 0.5*(out[:, 0, :, :] + out[:, 1, :, :])
-                    loss = obj_bce(cloud_pred, cloud_gt.to(device))
-                    loss += obj_bce(cloud_shadow_pred, cloud_shadow_gt.to(device))
+                    cloud_pred = out[:, 1, :, :] - out[:, 0, :, :]
+                    cloud_shadow_pred = out[:, 2, :, :] - out[:, 0, :, :]
+                    loss1 = obj_bce(cloud_pred, cloud_gt.to(device))
+                    loss2 = obj_bce(cloud_shadow_pred, cloud_shadow_gt.to(device))
+                    loss = loss1 + loss2
                 else:
                     cloud_gt = (batch[1] == 1).type(out.type())
-                    cloud_pred = out[:, 1, :, :] - out[:, 0:, :, :]
+                    cloud_pred = out[:, 1, :, :] - out[:, 0, :, :]
                     loss = obj_bce(cloud_pred, cloud_gt.to(device))
                 valid_losses.append(loss.item())
         avg_valid_loss = np.mean(valid_losses)
